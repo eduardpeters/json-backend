86b5074e171dacf0753481817dc4170079f03ba3
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-01-04 14:10:57 +0100

    Adding field data validation with regular expressions

--- helpers/controllerHelpers.js
+++ helpers/controllerHelpers.js
@@ -1,15 +1,35 @@
 const KEYS = ['name', 'publisher', 'alter_ego', 'first_appearance', 'image', 'characters'];
 
-const checkAll = reqBody => {
-    const bodyKeys = Object.keys(reqBody)
-    // Check all required keys are present and have data
-    return (KEYS.every(key => bodyKeys.includes(key) && reqBody[key]));
+const fieldRegex = key => {
+    switch (key) {
+        case ('image'):
+            return (/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/);
+        case ('first_appearance'):
+            return (/((\w+\s+)+(#[0-9]+)$)/);
+        case ('characters'):
+            return (/(\w+,?\s?)+(\w)$/);
+        default:
+            return (/(\w+\s*)+(\w)$/);
+    }
 }
 
+const checkField = (key, value) => {
+    const regex = fieldRegex(key);
+    return regex.test(value);
+}
+
+// Check all required keys are present and have valid data
+const checkAll = reqBody => {
+    const bodyKeys = Object.keys(reqBody)
+    return (KEYS.every(key => bodyKeys.includes(key) && checkField(key, reqBody[key])));
+}
+
+// Create an array with the valid keys provided, return it if data is valid
 const getValidFields = reqBody => {
-    const bodyKeys = Object.keys(reqBody);
-    // Return an array with the valid keys provided
-    return (bodyKeys.filter(key => KEYS.includes(key)));
+    const bodyKeys = Object.keys(reqBody).filter(key => KEYS.includes(key));
+    if (bodyKeys.every(key => checkField(key, reqBody[key])))
+        return (bodyKeys);
+    return ([]);
 }
 
 module.exports = { checkAll, getValidFields };
